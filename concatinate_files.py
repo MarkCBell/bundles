06a4d2989c24d1bd52d2936ccb9c6850f3ba3cc8
Mark Bell <mark00bell@googlemail.com>
2018-07-21 17:21:51 +0100

    Removed unneeded file.

--- concatinate_files.py
+++
@@ -1,7 +0,0 @@
-from census_generation.fileio import concatinate_files, line_count
-import os
-
-rel_path = '../Census/Parts/Word/'
-concatinate_files([os.path.join(rel_path, p) for p in os.listdir(rel_path)], '../Census/all_words.txt')
-
-print('Total words:', line_count('../Census/all_words.txt'))
