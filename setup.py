92a330131abd15751d30d2b6f0a88a23a4154cd8
Mark Bell <mark00bell@googlemail.com>
2018-07-22 11:13:26 +0100

    Moved out setup.py.

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages, Extension
+
+requirements = [
+    'pandas',
+    'numpy',
+    'decorator'
+]
+
+core_FSM = Extension(
+    name = 'bundle_ext.c_FSM_core',
+    sources = ['./bundle_ext/c_FSM_core.cpp'],
+    language='c++'
+    )
+core_matrix = Extension(
+    name = 'bundle_ext.c_matrix_core',
+    sources = ['./bundle_ext/c_matrix_core.cpp'],
+    language='c++'
+    )
+
+core_automorph = Extension(
+    name = 'bundle_ext.c_automorph_core',
+    sources = ['./bundle_ext/c_automorph_core.cpp'],
+    language='c++'
+    )
+
+setup(
+    name='bundler',
+    version='0.1.0',
+    description='',
+    author='Mark Bell',
+    author_email='mcbell@illinois.edu',
+    url='https://github.com/MarkCBell/bundles',
+    packages=find_packages(),
+    install_requires=requirements,
+    ext_modules=[core_FSM, core_matrix, core_automorph],
+    license='MIT License',
+    zip_safe=False,
+    keywords='bundler',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        ],
+)
