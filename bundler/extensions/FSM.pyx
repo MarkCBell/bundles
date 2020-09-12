8e3b5ec74a7ecc00b66143c66fad85c6f0c10740
Mark Bell <mark00bell@googlemail.com>
2020-09-12 21:55:18 +0100

    Added has_yield so can explicitly unroll the hit method.

--- bundler/extensions/FSM.pyx
+++ bundler/extensions/FSM.pyx
@@ -11,6 +11,7 @@ cdef class FSM:
     cdef int machine_len
     cdef dict yield_states
     cdef dict distance_to_yield
+    cdef array.array has_yield
     
     def __init__(self, alphabet, machine, yield_states):
         self.alphabet = alphabet
@@ -18,17 +19,17 @@ cdef class FSM:
         self.machine = array.array('i', machine)
         self.machine_len = len(self.machine) // self.alphabet_len
         self.yield_states = yield_states
+        self.has_yield = array.array('i', [1 if self.yield_states.get(state, []) else 0 for state in range(self.machine_len)])
         
-        num_states = len(self.machine) // len(self.alphabet)
         reverse_arrows = defaultdict(list)
-        for state in range(num_states):
+        for state in range(self.machine_len):
             for i in range(self.alphabet_len):
                 new_state = self.machine.data.as_ints[state * self.alphabet_len + i]
                 reverse_arrows[new_state].append(state)
         
         to_check = Queue()
         self.distance_to_yield = dict()
-        for state in range(num_states):
+        for state in range(self.machine_len):
             if self.yield_states.get(state, []):
                 self.distance_to_yield[state] = 0
                 to_check.put(state)
@@ -40,8 +41,7 @@ cdef class FSM:
                     self.distance_to_yield[adjacent] = self.distance_to_yield[current] + 1
                     to_check.put(adjacent)
     
-    def __call__(self, tuple word):
-        cdef int i, state = 0
+    def __call__(self, tuple word, int state=0):
         cdef int letter
         for letter in word:
             state = self.machine.data.as_ints[state * self.alphabet_len + letter]
@@ -54,14 +54,23 @@ cdef class FSM:
         return self.distance_to_yield[self(word)]
     
     def hit(self, tuple word):
-        ''' Return whether word meets any state that returns. '''
-        return any(self.hits(word))
+        ''' Return whether word meets any state that yields. '''
+        # return any(self.hits(word))
+        cdef int index, letter
+        cdef int state = 0
+        for index, letter in enumerate(word):
+            state = self.machine.data.as_ints[state * self.alphabet_len + letter]
+            if state < 0:
+                raise ValueError('Invalid transition to state {}'.format(state))
+            if self.has_yield.data.as_ints[state]:
+                return True
+        
+        return False
     
     def hits(self, tuple word):
         ''' Process word and yield (index, x) for all states that word hits that have things to yield. '''
-        cdef int i, index, state = 0
-        cdef int letter
-        # print(word)
+        cdef int index, letter
+        cdef int state = 0
         for index, letter in enumerate(word):
             state = self.machine.data.as_ints[state * self.alphabet_len + letter]
             if state < 0:
@@ -70,17 +79,18 @@ cdef class FSM:
                 yield (index+1, x)
 
     def has_cycle(self, tuple word, int depth=-1):
+        ''' Return whether there is a state such that self(word, state) == state. '''
         cdef array.array converted_word = array.array('i', word)
         cdef int l = len(word)
-        cdef int c, state, i, j
+        cdef int c, state, letter, i
         
         if depth < 0: depth = self.machine_len
         
         for c in range(depth):
             state = c
-            for j in range(l):
-                i = converted_word.data.as_ints[j]
-                state = self.machine.data.as_ints[state * self.alphabet_len + i]
+            for i in range(l):
+                letter = converted_word.data.as_ints[i]
+                state = self.machine.data.as_ints[state * self.alphabet_len + letter]
                 if state < 0: break
             
             if state == c:
