987e04145b40b25949f269ebdcc42e84092e52a4
Mark Bell <mark00bell@googlemail.com>
2020-09-12 21:11:56 +0100

    Automorph now uses Booths algo and low level C arrays.

--- bundler/extensions/automorphism.pyx
+++ bundler/extensions/automorphism.pyx
@@ -1,48 +1,159 @@
+from cpython cimport array
+import array
 
-cdef is_cyclic_ordered(tuple A, tuple B, bint prefix):
+from libc.stdlib cimport free, calloc
+
+cdef bint is_cyclic_ordered(int* A, int* B, int l, int* S, int* f):
     ''' Return whether A is <= all cyclic perumtations of B.
     
     Equivalent to:
         return all(A <= B[i:] + B[:i] for i in range(len(A))) '''
+    # Use Booths algorithm to find j, the best starting point. See:
+    # https://en.wikipedia.org/wiki/Lexicographically_minimal_string_rotation#Booth's_Algorithm
+    cdef int i, j = 0, last, ll = 2 * l
     
-    cdef int i, j, k, l = len(A)
-    for k in range(l):
-        j = k
-        for i in range(l):
-            j += 1
-            if j >= l:
-                if prefix:
-                    break  # !?! Recheck this.
-                else:
-                    j = 0
-            if A[i] < B[j]:
-                break
-            elif A[i] > B[j]:
-                return False
+    for i in range(l):  # S = B + B
+        S[i] = S[i+l] = B[i]
+    for i in range(ll):  # f = [-1] * len(S)
+        f[i] = -1
+    
+    for i in range(1, ll):
+        si = S[i]
+        last = f[i - j - 1]
+        while last != -1 and si != S[j + last + 1]:
+            if si < S[j + last + 1]:
+                j = i - last - 1
+            last = f[last]
+        if si != S[j + last + 1]:
+            if si < S[j]:  # j+last+1 = j
+                j = i
+            f[i - j] = -1
+        else:
+            f[i - j] = last + 1
+    
+    # Compare A to the (inplace) cycled B.
+    for i in range(l):
+        if A[i] < B[j]:
+            return True
+        elif A[i] > B[j]:
+            return False
+        j += 1
+        if j == l:  # Wrap around.
+            j = 0
     
     return True
 
 cdef class Automorph:
     cdef list alphabet
-    cdef list inverse
-    cdef list automorphisms
+    cdef int len_alphabet
+    cdef int len_alphabet1
     cdef int stop
+    cdef int num_automorphisms
+    cdef bint* any_missing
+    cdef bint* missing
+    cdef int** automorphisms
+    cdef int* inverse
     
     def __init__(self, list alphabet, list inverse, list automorphisms):
         cdef list missing, auto
+        cdef int i, j
         self.alphabet = alphabet
-        self.stop = len(self.alphabet)
-        self.inverse = inverse + [self.stop]
-        self.automorphisms = [(missing, auto + [self.stop]) for missing, auto in automorphisms]
+        self.len_alphabet = len(self.alphabet)
+        self.len_alphabet1 = self.len_alphabet + 1
+        self.stop = self.len_alphabet
+        self.inverse = <int *> calloc(self.len_alphabet1, sizeof(int))  # +1 for stop character.
+        for i in range(self.len_alphabet):
+            self.inverse[i] = inverse[i]
+        self.inverse[self.len_alphabet] = self.stop
+        
+        self.num_automorphisms = len(automorphisms)
+        self.any_missing = <bint *> calloc(self.num_automorphisms, sizeof(bint))
+        self.missing = <bint *> calloc(self.num_automorphisms * self.len_alphabet1, sizeof(bint))
+        for i in range(self.num_automorphisms):
+            if automorphisms[i][0]: self.any_missing[i] = 1
+            for j in automorphisms[i][0]:
+                self.missing[self.len_alphabet1 * i + j] = 1
+        self.automorphisms = <int **> calloc(self.num_automorphisms, sizeof(int*))
+        for i, (_, auto) in enumerate(automorphisms):
+            self.automorphisms[i] = <int *> calloc(self.len_alphabet1, sizeof(int))  # +1 for stop character.
+            for j in range(self.len_alphabet):
+                self.automorphisms[i][j] = auto[j]
+            self.automorphisms[i][self.len_alphabet] = self.stop
+    
+    def __del__(self):
+        cdef int i
+        free(self.any_missing)
+        free(self.missing)
+        for i in range(self.num_automorphisms):
+            free(self.automorphisms[i])
+        free(self.automorphisms)
+        free(self.inverse)
     
     def before_automorphs(self, tuple word, tuple next_word, bint prefix=False):
-        # Return whether word is before all cyclic permutations of all automorphs of next_word and next_word^-1.
-        cdef tuple other
-        cdef tuple next_word_inverse = tuple(self.inverse[letter] for letter in next_word[::-1])
-        for other in [next_word, next_word_inverse]:
-            for missing, automorphism in self.automorphisms:
-                if not (missing and prefix) and all(letter not in next_word for letter in missing) and not is_cyclic_ordered(word, tuple(automorphism[letter] for letter in other), prefix):
-                    return False
+        ''' Return whether word is before all cyclic permutations of all automorphs of next_word and next_word^-1. '''
+        cdef int l0 = len(word)
+        cdef int i, j, l = l0 + (1 if prefix else 0)
+        cdef bint bad
+        cdef int* automorphism
+        cdef int* wd = <int *> calloc(l, sizeof(int))
+        cdef int* nxt_wd = <int *> calloc(l, sizeof(int))
+        cdef int* nxt_wd_inv = <int *> calloc(l, sizeof(int))
+        cdef int* automorphed = <int *> calloc(l, sizeof(int))
         
-        return True
+        # Scratch memory for Booth's algorithm.
+        cdef int *tmp1 = <int *> calloc(2*l, sizeof(int))
+        cdef int *tmp2 = <int *> calloc(2*l, sizeof(int))
+        
+        try:
+            # Map word into a C array. Add a stop character to the end if required.
+            for i in range(l0):
+                wd[i] = word[i]
+            if prefix:
+                wd[l0] = self.stop
+            
+            # Map next_word into a C array. Add a stop character to the end if required.
+            for i in range(l0):
+                nxt_wd[i] = next_word[i]
+            if prefix:
+                nxt_wd[l0] = self.stop
+            
+            # Map next_word^-1 into a C array. Since nxt_wd already contains this, we reverse and inverse it.
+            for i in range(l):
+                nxt_wd_inv[i] = self.inverse[nxt_wd[l-i-1]]
+            
+            for i in range(self.num_automorphisms):
+                if prefix and self.any_missing[i]: continue
+                # if any(letter in next_word for letter in self.missing[i]): continue
+                bad = False
+                for j in range(l):
+                    if self.missing[self.len_alphabet1 * i + nxt_wd[j]] == 1:
+                        bad = True
+                        break
+                if bad: continue
+                automorphism = self.automorphisms[i]
+                
+                # Construct and test automorph(next_word).
+                for j in range(l):
+                    automorphed[j] = automorphism[nxt_wd[j]]
+                if not is_cyclic_ordered(wd, automorphed, l, tmp1, tmp2): return False
+                
+                # Construct and test automorph(next_word^-1).
+                for j in range(l):
+                    automorphed[j] = automorphism[nxt_wd_inv[j]]
+                if not is_cyclic_ordered(wd, automorphed, l, tmp1, tmp2): return False
+            
+            return True
+        finally:
+            free(wd)
+            free(nxt_wd)
+            free(nxt_wd_inv)
+            free(automorphed)
+            free(tmp1)
+            free(tmp2)
+        
+        # for missing, automorphism in self.automorphisms:
+            # if not (missing and prefix) and all(letter not in next_word for letter in missing) and not is_cyclic_ordered(word, tuple(automorphism[letter] for letter in other), prefix):
+                # return False
+        # 
+        # return True
 
